f2ec70841a6cbb93582f2147781a90148d839413
Elliot Glasenk <eglasenk@box.com>
2019-07-09 12:49:54 -0400

    Add front-end and dummy code for queue

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-console.log('test');
-
 function Elevator(id, minFloor, maxFloor, currentFloor) {
+  console.log('Elevator constructor');
   this.id = id;
   this.minFloor = minFloor;
   this.maxFloor = maxFloor;
@@ -11,10 +10,11 @@ function Elevator(id, minFloor, maxFloor, currentFloor) {
 //elevatorControlSystem
 //creation of the elevators
 function ElevatorControlSystem() {
+  console.log('ElevatorControlSystem constructor');
   this.availableElevators = [];
   this.createElevators = function() {
     for (let i = 0; i <= 2; i++) {
-      this.availableElevators.push(new Elevator(i, 0, 10, 0));
+      this.availableElevators.push(new Elevator('elevator' + (i + 1), 0, 10, 0));
     }
   };
 }
@@ -23,6 +23,7 @@ function ElevatorControlSystem() {
 //where last entry in destinationFloors is less than newFloor
 //to ensure efficient ride
 ElevatorControlSystem.prototype.queueNewDestination = function(newFloor) {
+  console.log('queueNewDestination method');
   let floorHasBeenAssignedToElevator = false;
   this.availableElevators.forEach(elevator => {
     if (
@@ -30,9 +31,10 @@ ElevatorControlSystem.prototype.queueNewDestination = function(newFloor) {
         elevator.destinationFloors[elevator.destinationFloors.length - 1] < newFloor) &&
       !floorHasBeenAssignedToElevator
     ) {
+      console.log('elevator.id', elevator.id);
       elevator.destinationFloors.push(newFloor);
+      document.getElementById(elevator.id + 'Queue').innerHTML = elevator.destinationFloors;
       floorHasBeenAssignedToElevator = true;
-      //break;
     }
   });
 };
@@ -43,11 +45,10 @@ ElevatorControlSystem.prototype.queueNewDestination = function(newFloor) {
 // 2) manual approach -- hey this trip to this floor for this elevator was complete
 
 Elevator.prototype.completeTripToFloor = function(floor) {
-  // console.log('completeTripToFloor');
-  // console.log('floor', floor);
-  // console.log('this.destinationFloors.indexOf(floor)', this.destinationFloors.indexOf(floor));
   if (this.destinationFloors.indexOf(floor) > -1) {
+    this.currentFloor = floor;
     this.destinationFloors.splice(this.destinationFloors.indexOf(floor), 1);
+    document.getElementById(this.id + 'Queue').innerHTML = this.destinationFloors;
   }
 };
 
@@ -55,3 +56,46 @@ Elevator.prototype.completeTripToFloor = function(floor) {
 //front end
 //input where you enter a value and it spits out
 //which elevator you'll be riding
+
+//commands:
+let elevatorControlSystem = new ElevatorControlSystem();
+elevatorControlSystem.createElevators();
+elevatorControlSystem.queueNewDestination(10);
+elevatorControlSystem.queueNewDestination(6);
+elevatorControlSystem.queueNewDestination(4);
+elevatorControlSystem.queueNewDestination(5);
+console.log('elevatorControlSystem.availableElevators', elevatorControlSystem.availableElevators);
+// setTimeout(() => {
+//   elevatorControlSystem.availableElevators[2].completeTripToFloor(4);
+// }, 1000);
+//console.log('elevatorControlSystem.availableElevators', elevatorControlSystem.availableElevators);
+
+// function destinationBtnClick() {
+//   console.log('destinationBtnClick');
+// }
+
+$('#destinationBtn').on('click', function(event) {
+  event.preventDefault(); // To prevent following the link (optional)
+  if (parseInt($('#destinationInput').val(), 10) <= 10) {
+    console.log('inside ifff');
+    elevatorControlSystem.queueNewDestination($('#destinationInput').val());
+  }
+});
+
+// setInterval(() => {
+//   let elevatorToUse = Math.floor(Math.random() * 3) + 1;
+//   console.log('elevatorToUse', elevatorToUse);
+//   console.log('elevatorToUse - 1', elevatorToUse - 1);
+//   let floorToComplete =
+//     elevatorControlSystem.availableElevators[elevatorToUse - 1].destinationFloors[0];
+//   console.log('floorToComplete', floorToComplete);
+//   if (floorToComplete) {
+//     elevatorControlSystem.availableElevators[elevatorToUse - 1].completeTripToFloor(
+//       floorToComplete
+//     );
+//     console.log(
+//       'elevatorControlSystem.availableElevators',
+//       elevatorControlSystem.availableElevators
+//     );
+//   }
+// }, 5000);
